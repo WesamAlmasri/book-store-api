a17fbc383a81d1324f3a4430f29a2c1b706d9556
Wesam Al-Masri <mr0virus@gmail.com>
2021-04-03 13:46:12 +0300

    test generic function

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .utils import JWTToken
 
-# Create your tests here.
+class TestGenericFunctions(APITestCase):
+
+    def test_get_random(self):
+        rand1 = JWTToken.get_random(10)
+        rand2 = JWTToken.get_random(10)
+        rand3 = JWTToken.get_random(15)
+
+        self.assertTrue(rand1)
+        self.assertNotEqual(rand1, rand2)
+        self.assertEqual(len(rand1), 10)
+        self.assertEqual(len(rand3), 15)
+    
+    def test_get_access_token(self):
+        payload = {
+            'id': 1
+        }
+
+        token = JWTToken.get_access(payload)
+        
+        self.assertTrue(token)
+    
+    def test_get_refresh_token(self):
+        refresh = JWTToken.get_refresh()
+        
+        self.assertTrue(refresh)
+
