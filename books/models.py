1653fb6161ae3c90dee397b05e6b05ee461caecc
Wesam Al-Masri <mr0virus@gmail.com>
2021-04-09 15:49:32 +0300

    books app serializers

--- books/models.py
+++ books/models.py
@@ -10,12 +10,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Auther(models.Model):
+    user = models.OneToOneField(CustomUser, related_name='auther_user', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     uploaded_by = models.OneToOneField(CustomUser, related_name='book_uploader', on_delete=models.CASCADE)
-    auther = models.ForeignKey(UserProfile, related_name='book_auther', on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, related_name='book_category', on_delete=models.CASCADE)
-    book = models.OneToOneField(FileUpload, related_name='book_file', on_delete=models.CASCADE)
-    cover_image = models.ForeignKey(FileUpload, related_name='book_cover', on_delete=models.CASCADE)
+    auther = models.ForeignKey(Auther, related_name='auther_books', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='category_books', on_delete=models.CASCADE)
+    file = models.OneToOneField(FileUpload, related_name='file_book', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     pages = models.IntegerField()
     description = models.TextField()
@@ -27,3 +36,21 @@ class Book(models.Model):
     
     class Meta:
         ordering = ('created_at',)
+
+class BookImage(models.Model):
+    book = models.ForeignKey(Book, related_name='book_images', on_delete=models.CASCADE)
+    image = models.ForeignKey(FileUpload, related_name='image_book', on_delete=models.CASCADE)
+    is_cover = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.book.auther.name} - {self.book.name} - {self.image}"
+
+class BookComment(models.Model):
+    book = models.ForeignKey(Book, related_name='book_comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name='user_comments', on_delete=models.CASCADE)
+    comment = models.TextField()
+    rate = models.IntegerField(default=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- books/serializers.py
+++ books/serializers.py
@@ -1,25 +1,51 @@
 from rest_framework import serializers
-from .models import Category, Book
+from .models import Category, Auther, Book, BookImage, BookComment
 from user.serializers import CustomUserSerializer, UserProfileSerializer, FileUploadSerializer
 
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = ('name',)
 
-class Book(serializers.ModelSerializer):
+class AutherSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Auther
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
     uploaded_by = CustomUserSerializer(read_only=True)
     uploaded_by_id = serializers.IntegerField(write_only=True)
-    auther = UserProfileSerializer(read_only=True)
+    auther = AutherSerializer(read_only=True)
     auther_id = serializers.IntegerField(write_only=True)
-    category = CustomUserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
-    book = FileUploadSerializer(read_only=True)
-    book_id = serializers.IntegerField(write_only=True)
-    cover_image = FileUploadSerializer(read_only=True)
-    cover_image_id = serializers.IntegerField(write_only=True)
-    
+    file = FileUploadSerializer(read_only=True)
+    file_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Book
         fields = '__all__'
+
+class BookImageSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    book_id = serializers.IntegerField(write_only=True)
+    image = FileUploadSerializer(read_only=True)
+    image_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = BookImage
+        fields = '__all__'
+
+class BookCommentSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    book_id = serializers.IntegerField(write_only=True)
+    user = CustomUserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = BookComment
+        dields = '__all__'
